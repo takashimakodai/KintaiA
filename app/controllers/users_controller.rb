628031463234bf12b6fd0bc2283706d6cf00f7a0
EC2 Default User <ec2-user@ip-172-31-32-159.ap-northeast-1.compute.internal>
2020-02-21 11:48:00 +0000

    Finish-show-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
   end
+  
+  def edit
+  end
+  
+  def create
+  end 
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
