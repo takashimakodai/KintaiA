34d3be405ddc8ca8fe131c1e18c445fadb8205d7
EC2 Default User <ec2-user@ip-172-31-32-159.ap-northeast-1.compute.internal>
2020-02-22 00:55:09 +0000

    Finish User-modeling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,26 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
+    @user = User.new
   end
-  
-  def edit
-  end
-  
+
   def create
-  end 
-  
-  def update
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
   end
-  
-  def destroy
-  end
-  
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
