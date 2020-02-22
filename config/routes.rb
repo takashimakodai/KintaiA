0d2d7fba4152ee817433ea3597c71c5c7e3cbd36
EC2 Default User <ec2-user@ip-172-31-32-159.ap-northeast-1.compute.internal>
2020-02-22 08:33:01 +0000

    login-cookies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
