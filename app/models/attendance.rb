a0cf37ebd155b4f87766ca3224eb1be7fe00f6a0
EC2 Default User <ec2-user@ip-172-31-32-159.ap-northeast-1.compute.internal>
2020-03-10 04:45:42 +0000

    Finish addittion-seven

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,11 +4,12 @@ class Attendance < ApplicationRecord
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
 
-  # 出勤時間が存在しない場合、退勤時間は無効
+  # 出勤時間が存在しない場合、退勤時間は無効(NO.7)
   validate :finished_at_is_invalid_without_a_started_at
-  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
+  
+  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効(NO.8)
   validate :started_at_than_finished_at_fast_if_invalid
-
+  
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
@@ -19,3 +20,5 @@ class Attendance < ApplicationRecord
     end
   end
 end
+
+  
